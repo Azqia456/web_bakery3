06aa21cb87e07d74fa21a35db0a31b25df16c364
aqilaauliah <19241552@bsi.ac.id>
2026-04-28 19:10:48 +0700

    membuat tombol logout

--- routes/web.php
+++ routes/web.php
@@ -5,36 +5,37 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\PesananController;
 use App\Http\Controllers\AuthController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
     return redirect()->route('login');
 });
+
 // Authentication Routes
 Route::middleware('guest')->group(function () {
     Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
     Route::post('/login', [AuthController::class, 'login']);
+
     Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
     Route::post('/register', [AuthController::class, 'register']);
 });
 
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth')->name('logout');
+Route::post('/logout', [AuthController::class, 'logout'])
+    ->middleware('auth')
+    ->name('logout');
 
 // Protected Routes
 Route::middleware('auth')->group(function () {
+
+    // OWNER
     Route::get('/dashboard', [DashboardController::class, 'index']);
-    Route::get('/api/dashboard/stats', [DashboardController::class, 'getStats']);
+
+    // PELANGGAN
     Route::get('/pelanggan/dashboard_pelanggan', [DashboardController::class, 'pelanggan']);
+
+    // API
+    Route::get('/api/dashboard/stats', [DashboardController::class, 'getStats']);
     Route::get('/api/pelanggan/dashboard_pelanggan/stats', [DashboardController::class, 'getPelangganStats']);
+
+    // PESANAN
     Route::get('/pelanggan/pesanan', [PesananController::class, 'pelangganView']);
     Route::get('/pesanan', [PesananController::class, 'view']);
 });
